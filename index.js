5a77063e3db9aafeaeb1e87ef503511ff5ab2b56
SocialBureauWeb <web@socialbureau.in>
2026-05-30 17:28:55 +0530

    Revanth 2

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    // Wait for database connection before starting server
     await connectDB();
     
     // Start cron jobs after DB is connected
